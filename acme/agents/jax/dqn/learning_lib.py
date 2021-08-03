8d036462e6ad846e730f46f577c19806e13c41f2
Khush Jammu <khushjammu@gmail.com>
2021-08-03 12:24:23 +0800

    changes

--- acme/agents/jax/dqn/learning_lib.py
+++ acme/agents/jax/dqn/learning_lib.py
@@ -153,7 +153,9 @@ class SGDLearner(acme.Learner):
   def step(self):
     """Takes one SGD step on the learner."""
     batch = next(self._data_iterator)
-    import pdb; pdb.set_trace()
+    # import ray; rapdb.set_trace()
+    print("FUCK YOU")
+    import sys; sys.exit(-1)
     self._state, extra = self._sgd_step(self._state, batch)
 
     if self._replay_client:
