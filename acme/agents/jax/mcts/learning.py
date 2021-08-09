223f3c55a7d0457714672e7b8cbb5d977b33efac
Khush Jammu <khushjammu@gmail.com>
2021-08-09 16:23:29 +0800

    changes

--- acme/agents/jax/mcts/learning.py
+++ acme/agents/jax/mcts/learning.py
@@ -95,7 +95,8 @@ class MCTSLoss(LossFn):
     """Calculate a loss on a single batch of data."""
     del key
     transitions: types.Transition = batch.data
-    # print(batch.data)
+    print(transitions)
+    print(batch.data)
     print(batch.info) # need to figure out how to get the pi
     keys, probs, *_ = batch.info
 
