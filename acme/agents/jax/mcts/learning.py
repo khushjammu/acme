21c2e83bd5ae84c31a59160118c3f6a8069ab5ba
Khush Jammu <khushjammu@gmail.com>
2021-08-10 19:31:26 +0800

    backup

--- acme/agents/jax/mcts/learning.py
+++ acme/agents/jax/mcts/learning.py
@@ -218,7 +218,7 @@ class MCTSLearner(acme.Learner):
     self._state, extra = self._sgd_step(self._state, batch)
     new = copy.deepcopy(self._state.params)
 
-    if old == new:
+    if (old == new).all():
       print("doesn't update")
     else:
       print("updated!")
